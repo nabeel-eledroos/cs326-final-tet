6cb28cd8ba8b31dc5953f7f208b1ead51a160d0d
Jake Recoulle <42967542+jdrecoulle0001@users.noreply.github.com>
2020-11-08 15:02:46 -0500

    Added GET Request for app page, which returns json of the top and most popular stories. Added files with fake data for now.

---
+++ public/scripts/app.js
@@ -0,0 +1,42 @@
+
+const __dirname = location.protocol + '//' + location.host;
+
+async function getTopStories() {
+    try {
+        const response = await fetch(__dirname + '/topStories');
+
+        if(response.ok) {
+            const topStoriesJSON = await response.json();
+            return topStoriesJSON;
+        } else {
+            throw 'Problem fetching from server: ' + response.statusText;
+        }
+    } catch(e) {
+        alert(e);
+        return { topStoriesResults: [] };
+    }
+}
+
+async function getMostPopular() {
+    try {
+        const response = await fetch(__dirname + '/mostPopular');
+
+        if(response.ok) {
+            const mostPopularJSON = await response.json();
+            return mostPopularJSON;
+        } else {
+            throw 'Problem fetching most popular stories from the server: ' + response.statusText;
+        }
+    } catch(e) {
+        alert(e);
+        return { mostPopularResults: [] };
+    }
+}
+
+window.addEventListener('load', async () => {
+    const topStories = await getTopStories();
+    console.log(topStories);
+    
+    const mostPopular = await getMostPopular();
+    console.log(mostPopular);
+});
