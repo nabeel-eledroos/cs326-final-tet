210d8534f90a33e674eff5ace7131616d8d213e9
Jake Recoulle <42967542+jdrecoulle0001@users.noreply.github.com>
2020-11-07 20:09:52 -0500

    Created handler for submitting interests form. Handler gets form info and sends POST to server. Response is json

---
+++ public/scripts/interests.js
@@ -0,0 +1,32 @@
+const __dirname = location.protocol + '//' + location.host;
+
+window.addEventListener('load', () => {
+    document.getElementById('interests-submit').addEventListener('click', async () => {
+        const formInputs = document.forms['interests-form'].getElementsByTagName('input');
+        const interests = Array.from(formInputs).reduce((acc, box) => {
+            if(box.checked) {
+                acc.push(box.labels[0].innerText);
+            }
+            return acc;
+        }, []);
+        console.log(interests);
+
+        const body = { interests: interests };
+        const response = await fetch(__dirname + '/interests', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if(response.ok) {
+            const status = await response.json();
+            console.log(status);
+            window.location.hred = '../app/app.html';
+        } else {
+            console.log(response.error);
+            alert('Could not reach out to the server! Sorry.');
+        }
+    });
+});
