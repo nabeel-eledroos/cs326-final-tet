fbc24b3d6a28f0297a20fcb340a7fa104296ef5b
Nabeel Eledroos <nabeel@eledroos.net>
2020-11-18 22:34:34 -0500

    rendering relevent charities in trending topics

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -55,12 +55,45 @@ async function getCharities() {
     }
 }
 
+async function searchCharities(param) {
+    try {
 
-function render(articles) {
+        const body = { param:param };
+        const response = await fetch(__dirname + '/charitySearch', {
+            method: 'post',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if(response.ok) {
+            const charitiesJSON = await response.json();
+            return charitiesJSON;
+        } else {
+            throw 'Problem fetching from the server: ' + response.statusText;
+        }
+    } catch(e) {
+        alert(e);
+        return { mostPopularResults: [] };
+    }
+}
+
+
+async function render(articles) {
     // Picking top 3 articles from result (how else should we pick articles?)
     for (let i=1;i<=3;i++) {
-        document.getElementById(`card-${i}-title`).innerHTML = "<a href='" + articles[i].url + "' target='_blank'>" + articles[i].title + " </a> <i class='fas fa-" + icons[articles[i].section] + "'></i>";
+        document.getElementById(`card-${i}-title`).innerHTML = `<a href='${articles[i].url}' target='_blank'>${articles[i].title} </a> <i class='fas fa-${icons[articles[i].section]}'></i>`;
         document.getElementById(`card-${i}-text`).innerText = articles[i].section;
+
+        // Searching for charities by article section
+        console.log(articles[i].section);
+        //TODO: edge case if search comes back empty
+        const charities = await searchCharities(articles[i].section);
+        // console.log(charities);
+        for (let j=1;j<=3;j++) {
+            document.getElementById(`card-${i}-charity-${j}`).innerText = charities[j].charityName;
+        }
     }
 }
 
@@ -68,9 +101,7 @@ window.addEventListener('load', async () => {
     // const topStories = await getTopStories();
     // console.log(topStories);
 
-    // const mostPopular = await getMostPopular();
+    const mostPopular = await getMostPopular();
     // console.log(mostPopular);
-    // render(mostPopular.results);
-    const charities = await getCharities();
-    console.log(charities);
+    await render(mostPopular.results);
 });
