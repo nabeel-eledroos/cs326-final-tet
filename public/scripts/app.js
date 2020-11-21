bacd9f7a3906b7f0c8ce74fe8f1af7e48b3c5f1a
Nabeel Eledroos <nabeel@eledroos.net>
2020-11-21 12:23:27 -0500

    enhanced trending topics rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,8 @@
 
 const __dirname = location.protocol + '//' + location.host;
 
-const icons = { "Arts": "palette", "Automobiles": "car", "Books": "book", 'Business': "user-tie", "Fashion": "tshirt", "Food": "hamburger",
-    "Health": "heartbeat", "Home": "star", "Insider": "user-secret", "Magazine": "newspaper", "Movies": "video", "Nyregion": "map-pin", "Obituaries": "skull-crossbones",
+const icons = { "Arts": "palette", "Automobiles": "car", "Books": "book", 'Business': "user-tie", "Fashion": "tshirt", "Style":"tshirt", "Food": "hamburger",
+    "Health": "heartbeat", "Home": "star", "Insider": "user-secret", "Magazine": "newspaper", "Movies": "video", "New York": "map-pin", "Obituaries": "skull-crossbones",
     "Opinion": "pen", "Politics": "landmark", "Realestate": "home", "Science": "virus", "Sports": "futbol", "Sundayreview": "question", "Technology": "tv",
     "Theater": "theater-masks", "T-magazine": "route", "Travel": "route", "Upshot": "question", "U.S.": "flag-usa", "World" : "globe-americas"
 }
@@ -57,7 +57,6 @@ async function getCharities() {
 
 async function searchCharities(param) {
     try {
-
         const body = { param:param };
         const response = await fetch(__dirname + '/charitySearch', {
             method: 'post',
@@ -80,22 +79,43 @@ async function searchCharities(param) {
 }
 
 
-async function render(articles) {
-    // Picking top 3 articles from result (how else should we pick articles?)
-    for (let i=1;i<=3;i++) {
-        document.getElementById(`card-${i}-title`).innerHTML = `<a href='${articles[i].url}' target='_blank'>${articles[i].title} </a> <i class='fas fa-${icons[articles[i].section]}'></i>`;
-        document.getElementById(`card-${i}-text`).innerText = articles[i].section;
+async function render_trending(articles) {
+    // Picking a random number (1-17) and getting 3 articles
+    const article_start = Math.floor(Math.random() * 17) + 1;
+    let card_num = 1;
+    // console.log(articles);
+
+    for (let i=article_start;i<article_start+3;i++) {
+        document.getElementById(`card-${card_num}-title`).innerHTML = `<a href='${articles[i].url}' target='_blank'>${articles[i].title} </a> <i class='fas fa-${icons[articles[i].section]}'></i>`;
+        document.getElementById(`card-${card_num}-text`).innerText = articles[i].section;
 
         // Searching for charities by article section
-        console.log(articles[i].section);
         //TODO: edge case if search comes back empty
         const charities = await searchCharities(articles[i].section);
         console.log(charities);
-        for (let j=1;j<=3;j++) {
-            document.getElementById(`card-${i}-charity-${j}`).innerHTML = `<a href='${charities[j].charityNavigatorURL}' target='_blank'>${charities[j].charityName} </a>`;
+        // Picking a random number (1-17) and getting 3 charities
+        const charity_start = Math.floor(Math.random() * 17) + 1;
+        let charity_num = 1;
+        for (let j=charity_start;j<charity_start+3;j++) {
+            document.getElementById(`card-${card_num}-charity-${charity_num}`).innerHTML = `<a href='${charities[j].charityNavigatorURL}' target='_blank'>${charities[j].charityName} </a>`;
+            charity_num ++;
         }
+        card_num ++;
     }
 }
+
+// async function render_causes(articles) {
+//     //TODO: edge case if search comes back empty
+//     const charities = await searchCharities(articles[i].section);
+//     console.log(charities);
+//     // Picking a random number (1-17) and getting 3 charities
+//     const charity_start = Math.floor(Math.random() * 17) + 1;
+//     let charity_num = 1;
+//     for (let j=charity_start;j<charity_start+3;j++) {
+//         document.getElementById(`card-${card_num}-charity-${charity_num}`).innerHTML = `<a href='${charities[j].charityNavigatorURL}' target='_blank'>${charities[j].charityName} </a>`;
+//         charity_num ++;
+//     }
+// }
 
 window.addEventListener('load', async () => {
     // const topStories = await getTopStories();
@@ -103,5 +123,11 @@ window.addEventListener('load', async () => {
 
     const mostPopular = await getMostPopular();
     // console.log(mostPopular);
-    await render(mostPopular.results);
+    await render_trending(mostPopular.results);
+    // await render_causes(mostPopular.results);
 });
+
+// document.getElementById('.cause-filter').addEventListener('change', (event) => {
+//   const result = document.querySelector('.result');
+//   result.textContent = `You like ${event.target.value}`;
+// });
